8926d31f3bd6470f1bfdce96664e099c794c780b
YURI <141793568+Yul-ia@users.noreply.github.com>
2024-06-26 17:46:33 +0900

    4-dimensional land-sea masking file_NCEP_airtemper_masking landsea & weighted.py

---
+++ NCEP_Reanalysis II /weighted/3.NCEP_airtemper_masking landsea & weighted.py
@@ -0,0 +1,35 @@
+import os
+import xarray as xr
+import numpy as np
+import glob
+import pandas as pd
+import datetime
+from dateutil.relativedelta import relativedelta
+
+os.getcwd()
+os.chdir('')
+
+air_temp = xr.open_dataset('air.2m.mon.mean.nc') 
+mask_file = xr.open_dataset('landsea.nc') 
+
+air = air_temp.air
+land = mask_file.land
+air['land'] =land
+
+lenght_air = len(air)
+
+lst = []
+time_for_df =[]
+weight = np.cos(np.deg2rad(air[0].lat))
+
+for i in range(lenght_air):
+    air_weight = air[i].where(air[0].land==0).weighted(weight) 
+    result = (air_weight.mean(('lon','lat'))-273.15).values[0] # Unit conversion [K] > [°C]
+    air_time = str(air[i].time.values)[:10]
+    
+    lst.append(result)
+    time_for_df.append(air_time)
+
+ 
+new_df = pd.DataFrame({'time':time_for_df,'air_weight':lst})
+new_df.to_csv('air_temper_weight.csv',index=False)
