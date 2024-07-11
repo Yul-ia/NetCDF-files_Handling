2b24f0712def63933a015c356070632775d41e29
YURI <141793568+Yul-ia@users.noreply.github.com>
2024-07-11 16:13:03 +0900

    change unit conversion

--- NCEP_Reanalysis II /4. Weighted_Average_Specific_Region /4.NCEP_pres_east_asia_mask&weighted.py
+++ NCEP_Reanalysis II /4. Weighted_Average_Specific_Region /4.NCEP_pres_east_asia_mask&weighted.py
@@ -5,27 +5,34 @@ import numpy as np
 import datetime
 from dateutil.relativedelta import relativedelta
 
+os.getcwd()
 os.chdir('')
 
 ds_pres = xr.open_dataset('pres.sfc.mon.mean.nc')
 mask_file = xr.open_dataset('landsea.nc')
-asia_mask_file = xr.open_dataset('')
+asia_mask_file = xr.open_dataset('ASKmaskNew20230615.nc')
 
-pres = ds_pres.pres
+# Unit # Unit conversion: pa -> hpa
+pa_to_hpa = 0.01 # [hPa]
+
+pres = ds_pres.pres * pa_to_hpa
 land = mask_file.land
-landsea = asia_mask_file.land
+landsea = asia_mask_file.LANDSEA
 
 ### grid 
-pres_sel = ds_pres.sel(lon=slice(), lat=slice())
+pres_sel = ds_pres.sel(lon=slice(110,150), lat=slice(53.5,20))
 
 asia_lon = asia_mask_file.XA # lon
 asia_lat = asia_mask_file.YA # lat
 
-sel_new_grid = pres_sel.interp(lon=asia_lon, lat= asia_lat) # Grid adjustment
+sel_new_grid = pres_sel.interp(lon=asia_lon, lat= asia_lat) 
 
 ### mask
 sel_new_grid['landsea']=landsea # east_asia_mask // dataset
 pres['land'] = land # glob mask // data array
+
+
+sel_new_grid_pres = sel_new_grid.pres*pa_to_hpa # Unit conversion: pa -> hpa
 
 ### weighted 
 weight_asia = np.cos(np.deg2rad(sel_new_grid.lat))
@@ -35,45 +42,51 @@ weight_glob = np.cos(np.deg2rad(pres.lat))
 length_pres = len(sel_new_grid.pres)
 
 glb_lst = []
-es_lst = [] 
-ys_lst = [] 
-ecs_lst=[] 
-ask_lst =[] 
+es_lst = [] # 3
+ys_lst = [] # 1
+ecs_lst=[] # 2
+ask_lst =[] #!=0 
 
 time_lst = []
 
-# Unit # Unit conversion: pa -> hpa
-pa_to_hpa = 0.01 # [hPa]
 
 for i in range(length_pres):
-    ys_weight = sel_new_grid.pres[i].where(sel_new_grid.landsea == '').weighted(weight_asia)
-    result = ys_weight.mean(('lon','lat')).values
-    ys_lst.append(result*pa_to_hpa)
+    ys_weight = sel_new_grid_pres[i].where(sel_new_grid.landsea == 1).weighted(weight_asia)
+    result = ys_weight.mean(('XA','YA')).values
+    ys_lst.append(result)
     
-    ecs_weight = sel_new_grid.pres[i].where(sel_new_grid.landsea == '').weighted(weight_asia)
-    result2 =ecs_weight.mean(('lon','lat')).values
-    ecs_lst.append(result2*pa_to_hpa)
+    ecs_weight = sel_new_grid_pres[i].where(sel_new_grid.landsea == 2).weighted(weight_asia)
+    result2 =ecs_weight.mean(('XA','YA')).values
+    ecs_lst.append(result2)
     
-    es_weight = sel_new_grid.pres[i].where(sel_new_grid.landsea == '').weighted(weight_asia)
-    result3 = es_weight.mean(('lon','lat')).values
-    es_lst.append(result3*pa_to_hpa)
+    es_weight = sel_new_grid_pres[i].where(sel_new_grid.landsea == 3).weighted(weight_asia)
+    result3 = es_weight.mean(('XA','YA')).values
+    es_lst.append(result3)
     
-    ask_weight = sel_new_grid.pres[i].where(sel_new_grid.landsea == '').weighted(weight_asia)
-    result4 = ask_weight.mean(('lon','lat')).values
-    ask_lst.append(result4*pa_to_hpa)
+    ask_weight = sel_new_grid_pres[i].where(sel_new_grid.landsea !=0 ).weighted(weight_asia)
+    result4 = ask_weight.mean(('XA','YA')).values
+    ask_lst.append(result4)
     
-    glb_weight = pres[i].where(pres[0].land==0).weighted(weight_glob)
+    
+    # glb_weight = pres_sel.pres[i].where(pres[0].land == 0).weighted(weight_glob)
+    glb_weight = pres[i].where(pres[0].land==0).weighted(weight_glob) ### 위에랑 결과 다른지 비교
     result5 = glb_weight.mean(('lon','lat')).values
-    glb_lst.append(result5*pa_to_hpa)
+    glb_lst.append(result5)
     
     time_lst.append(str(sel_new_grid.time[i].values)[:10])
     
+
+
 new_df = pd.DataFrame({'date':time_lst, 'glb_mean':glb_lst, 'ask_mean':ask_lst,
                        'es_mean':es_lst, 'ys_mean':ys_lst, 'ecs_mean': ecs_lst})
-new_df=new_df.round(2)
+new_df.reset_index(drop=True, inplace=True)
+
 new_df
 
 os.getcwd()
-# new_df.to_csv('pres_asia_mask&weighted.csv',index=False)
+new_df.to_csv('pres_asia_mask&weighted.csv',index=False)
 
-     
+    
+    
+    
+    
