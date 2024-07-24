cec69b3b9ed95d3a7c06548ab93f01089a4d04f6
YURI <141793568+Yul-ia@users.noreply.github.com>
2024-07-24 17:49:27 +0900

    5.CMAP_precip_longterm_east_asia_mask&weight.py

---
+++ CMAP /5. Weighted_Average_Global&Specific_Region /5.CMAP_precip_longterm_east_asia_mask&weight.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import os
+import datetime
+from dateutil.relativedelta import relativedelta
+
+os.chdir('')
+### new clim file
+ds_precip = xr.open_dataset('CMAP_longterm/CMAP_precip_mon_clim_9120.nc') # [mm/day] > [mm/month]
+
+mask_file = xr.open_dataset('CPC_landsea.nc')  # glb mask for CMAP
+asia_mask_file = xr.open_dataset('ASKmaskNew20230615.nc') # asia mask
+
+# Unit : mm/day -> mm/month
+for i in range(len(ds_precip.pre_clim)):
+    p_time = str(ds_precip.month[i].values)
+    b = datetime.datetime.strptime(p_time,'%m')
+    nums_of_day = ((b + relativedelta(months=i))-datetime.timedelta(days=1)).day
+    ds_precip.pre_clim[i] *= nums_of_day
+
+precip = ds_precip.pre_clim
+
+land = mask_file.landsea # cpc mask
+landsea = asia_mask_file.LANDSEA
+
+## for glb grid 
+new_sel = precip.sel(lon=slice(0,357.5), lat = slice(90,-90))
+glb_lon = mask_file.X # lon
+glb_lat = mask_file.Y # lat
+glb_new_grid = new_sel.interp(lon = glb_lon, lat = glb_lat)
+
+## for asia grid
+precip_sel = precip.sel(lon=slice(110,150), lat = slice(53.5,20))
+asia_lon = asia_mask_file.XA # lon
+asia_lat = asia_mask_file.YA # lat
+asia_new_grid = precip_sel.interp(lon = asia_lon, lat = asia_lat)
+
+### mask
+asia_new_grid['landsea'] = landsea # asia mask
+glb_new_grid['land'] = land # glb mask
+
+### weighted
+weight_asia = np.cos(np.deg2rad(asia_new_grid.lat))
+weight_glob = np.cos(np.deg2rad(glb_new_grid.lat))
+
+
+### 
+length_precip= len(asia_new_grid)
+
+glb_lst = []
+es_lst = [] # 3
+ys_lst = [] # 1
+ecs_lst=[] # 2
+ask_lst =[] #!=0 
+
+time_lst = []
+
+
+for i in range(length_precip):
+    p_time = str(glb_new_grid.month[i].values)
+    
+    yel_weight = asia_new_grid[i].where(asia_new_grid.landsea==1).weighted(weight_asia)
+    result = yel_weight.mean(('XA', 'YA')).values
+    ys_lst.append(result)
+    
+    
+    echina_weight = asia_new_grid[i].where(asia_new_grid.landsea==2).weighted(weight_asia)
+    result2 = echina_weight.mean(('XA', 'YA')).values
+    ecs_lst.append(result2)
+    
+    esea_weight = asia_new_grid[i].where(asia_new_grid.landsea==3).weighted(weight_asia)
+    result3 = esea_weight.mean(('XA', 'YA')).values
+    es_lst.append(result3)
+
+    easiasea_weight = asia_new_grid[i].where(asia_new_grid.landsea!=0).weighted(weight_asia) 
+    result4 = easiasea_weight.mean(('XA', 'YA')).values
+    ask_lst.append(result4)
+    
+    
+    glob_weight = glb_new_grid[i].where(glb_new_grid[0].land==0).weighted(weight_glob)
+    result5 = glob_weight.mean(('X','Y')).values
+    glb_lst.append(result5)
+    
+    
+    time_lst.append(p_time)
+
+new_df = pd.DataFrame({'month':time_lst,'glb_clim':glb_lst, 'es_clim':es_lst,'ys_clim':ys_lst,
+                       'ecs_clim':ecs_lst,'ask_clim':ask_lst})
+
+new_df.reset_index(drop=True, inplace=True)
+new_df
+
+os.getcwd()
+new_df.to_csv('precip_new_lonterm_east_asia_mask&weighted.csv',index=False)
